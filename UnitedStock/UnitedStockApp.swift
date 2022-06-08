db68a84c0881a0d4c1dd6025bd487a0dc5f8bfc3
Irvin <irvin999rafael6@csu.fullerton.edu>
2022-06-08 11:13:29 -0700

    Initial Commit

---
+++ UnitedStock/UnitedStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitedStockApp.swift
+//  UnitedStock
+//
+//  Created by csuftitan on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitedStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
