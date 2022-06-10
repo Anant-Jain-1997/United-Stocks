b7245a534dce0f8b79cffd88398bfccab4f7c5da
Irvin <irvin999rafael6@csu.fullerton.edu>
2022-06-10 15:47:00 -0700

    Irvin - Created a User class with two initializers, and some member variables

--- UnitedStock/UnitedStockApp.swift
+++ UnitedStock/UnitedStockApp.swift
@@ -15,3 +15,31 @@ struct UnitedStockApp: App {
         }
     }
 }
+
+class User {
+    //if they fill out all the information then they will change member var to the information provided
+    init(name: String, age: Int, password: String, username: String, favstock: String) {
+        self.name = name
+        self.age = age
+        self.password = password
+        self.username = username
+        self.favstock = favstock
+    }
+    
+    //this initializer is the same, except if the user provides no favorite stock it is left as blank
+    init(name: String, age: Int, password: String, username: String) {
+        self.name = name
+        self.age = age
+        self.password = password
+        self.username = username
+        self.favstock = ""
+    }
+    
+    
+    //user variables
+    var username: String
+    var password: String
+    var name: String
+    var age: Int
+    var favstock: String
+}
