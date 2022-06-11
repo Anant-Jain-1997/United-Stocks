f5248fbe1b200ad5e6350bddfde44846629afbaa
Marius Iacob <mariusiacob@csu.fullerton.edu>
2022-06-11 09:49:11 -0700

    Added: Watchlist class, methods, and properties

--- UnitedStock/Watchlist.swift
+++ UnitedStock/Watchlist.swift
@@ -7,7 +7,55 @@
 
 import Foundation
 
-struct Watchlist
+class Watchlist
 {
-    // add code here
+    // Properties
+    var stockName: String
+    var symbol: String
+    var about: String
+    var price: Double
+    
+    // Initializer (constructor)
+    init(stockName: String, symbol: String, about: String, price: Double)
+    {
+        self.stockName = stockName
+        self.symbol    = symbol
+        self.about     = about
+        self.price     = price
+    }
+    
+    // Overloaded Initializer (Do we need??)
+    
+    
+    // Deinitializer (destructor)
+
+    
+    // Methods
+    func addStockWatchlist () -> Bool // Return is Bool because it checks if stock is successfuly add (returns true)
+                                   // if failed (return false)
+    {
+        // dummy return
+        return true
+    }
+        
+    func removeStockWatchlist () -> Void // Removes the stock from the watchlist
+    {
+        
+    }
+    
+    func createNewWatchlist () -> Void // Create more than one wathchlist
+    {
+        
+    }
+    
+    func deleteWatchlist () -> Void // Deletes a specific watchlist
+    {
+        
+    }
+    
+    func filterWatchlist () -> Void // Filters the watchlist by low/high price
+    {
+        
+    }
+    
 }
