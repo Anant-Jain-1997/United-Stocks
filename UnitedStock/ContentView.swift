50352dacece80cd0ccadc8be542e2849b5112f43
Sergio Murguia <smurguia1@csu.fullerton.edu>
2022-06-13 22:51:29 -0700

    MY basic UI code for Searching the API

--- UnitedStock/ContentView.swift
+++ UnitedStock/ContentView.swift
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var searchText = " "
+    let myStocks = [
+    "APPLE INC $129.67","Tesla $399.23 ","PEPSI CO  $127.45"
+    ,"Google INC $210.35",]
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack(alignment: .leading) {
+            ZStack {
+                Rectangle()
+                    .foregroundColor(Color("DarkGray"))
+                HStack{
+                    Image(systemName: "magnifyingglass")
+                    TextField("Search..", text: $searchText)
+                }
+                    .foregroundColor(.gray)
+                    .padding(.leading,13)
+            }
+            .frame(height: 50 )
+            .cornerRadius(13)
             .padding()
+                
+                List(myStocks,id: \.self){ stocks in
+                    Text(stocks)
+                
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Stocks")
+        }
+    }
+    }
+struct SearchBar: View {
+    @Binding var searchText: String
+    var body: some View{
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color("LightGray"))
+            HStack{
+                Image(systemName: "magnifyingglass")
+                TextField("Search... ", text: $searchText)
+            }
+            .foregroundColor(.gray)
+            .padding(.leading,13)
+        }
+        .frame(height: 40)
+        .cornerRadius(13)
+        .padding()
     }
 }
 
