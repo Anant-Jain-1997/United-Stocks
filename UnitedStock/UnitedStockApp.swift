501b88f5b254ceca355c5a5a643fae9b3725c61b
Irvin <irvin999rafael6@csu.fullerton.edu>
2022-06-15 11:46:19 -0700

    Irvin - created a separate file, UserManagement.swift and moved the contents of UnitedStockApp.swift to the new file

--- UnitedStock/UnitedStockApp.swift
+++ UnitedStock/UnitedStockApp.swift
@@ -16,43 +16,4 @@ struct UnitedStockApp: App {
     }
 }
 
-class User {
-    //initializer sets all member variables
-    init(name: String, age: Int, password: String, username: String, favStock: String) {
-        self.name = name
-        self.age = age
-        self.password = password
-        self.username = username
-        self.favStock = favStock
-    }
-    
-    //this initializer is the same, except if the user provides no favorite stock it is left as blank
-    init(name: String, age: Int, password: String, username: String) {
-        self.name = name
-        self.age = age
-        self.password = password
-        self.username = username
-        self.favStock = ""
-    }
-    
-    
-    //user variables
-    var username: String
-    var password: String
-    var name: String
-    var age: Int
-    var favStock: String
-    
-    func changeFavStock(to favStock: String) -> Void {
-        self.favStock = favStock
-    }
-    func changeName(to name: String) -> Void {
-        self.name = name
-    }
-    func changeAge(username: String) -> Void {
-        self.username = username
-    }
-    func changePassword(password: String) -> Void {
-        self.password = password
-    }
-}
+
