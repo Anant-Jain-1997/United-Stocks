864ec24fdd81a87e90045e8b748d6ff2cdd1d73c
Irvin <irvin999rafael6@csu.fullerton.edu>
2022-06-20 20:56:05 -0700

    Irvin - committing and pushing ContentView they did not get pushed in last commit

--- UnitedStock/ContentView.swift
+++ UnitedStock/ContentView.swift
@@ -8,63 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var name: String = ""
-    @State var username: String = ""
-    @State var age: String = ""
-    @State var favStock: String = ""
+    @StateObject var user = User(name: "", age: 0, password: "", username: "")
     var body: some View {
-        VStack {
-            Text("United Stock Account")
-            HStack {
-                Spacer()
-                Text("Name: ")
-                TextField("Name", text: $name)
-                Spacer()
-            }
-            HStack {
-                Spacer()
-                Text("Username: ")
-                TextField("Username", text: $username)
-                Spacer()
-            }
-            HStack {
-                Spacer()
-                Text("Age: ")
-                TextField("Age", text: $age)
-                Spacer()
-            }
-            HStack {
-                Spacer()
-                Text("Favorite Stock: ")
-                TextField("Favorite Stock", text: $favStock)
-                Spacer()
-            }
-            Button(action: {
-                print("Added User!")
-                }) {
-                Text("Add User")
-                }.padding()
-            Text("Already a User?")
-            Button(action: {
-                print("Signed Existing User!")
-                }) {
-                Text("Sign In")
-                }.padding()
-            
-            VStack {
-                Spacer()
-                Text("User Layout")
-                Text( "Name :  \(name)" )
-                Text( "Username :  \(username)" )
-                Text( "Age :  \(age)" )
-                Text( "Favorite Stock :  \(favStock)" )
-                Button(action: {
-                    print("Changed User Settings!")
-                    }) {
-                    Text("Change User Info")
-                    }.padding()
-            }
-            }
+        UserSignIn()
     }
 }
 
