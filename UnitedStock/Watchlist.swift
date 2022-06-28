46a4c86c26ee985b6ba268b9644f733379d313e2
Marius Iacob <mariusiacob@csu.fullerton.edu>
2022-06-27 23:17:02 -0700

    Check-in 4: Attempted to implemented data persistence to store data. Got some features to work

--- UnitedStock/Watchlist.swift
+++ UnitedStock/Watchlist.swift
@@ -7,10 +7,10 @@
 //
 
 import Foundation
-import SwiftUI
+//import SwiftUI
 
-struct WatchlistProps: Identifiable{
-    //Properties
+struct StockProps: Identifiable {
+    // Properties
     var id = UUID()
     var stockName: String
     var symbol: String
@@ -18,39 +18,34 @@ struct WatchlistProps: Identifiable{
     var price: String
 }
 
-class Watchlist: ObservableObject{
-    @Published var stocks: [WatchlistProps] = []
+class Watchlist: ObservableObject {
+    @Published var stocks: [StockProps] = []
     
     // Initializer (constructor)
-    init(){
-        stocks.append(WatchlistProps(stockName: "Apple", symbol: "APPL", description: "Tech stock", price: "$131.56"))
-        stocks.append(WatchlistProps(stockName: "Lucid Motors", symbol: "LCID", description: "EV stock", price: "$16.63"))
-        stocks.append(WatchlistProps(stockName: "Google", symbol: "GOOG", description: "Tech stock", price: "$2,157.31"))
-        stocks.append(WatchlistProps(stockName: "Nvidia", symbol: "NVDA", description: "Tech stock", price: "$158.80"))
+    init() {
+        stocks.append(StockProps(stockName: "Apple", symbol: "APPL", description: "Tech stock", price: "$131.56"))
+        stocks.append(StockProps(stockName: "Lucid Motors", symbol: "LCID", description: "EV stock", price: "$16.63"))
+        stocks.append(StockProps(stockName: "Google", symbol: "GOOG", description: "Tech stock", price: "$2,157.31"))
+        stocks.append(StockProps(stockName: "Nvidia", symbol: "NVDA", description: "Tech stock", price: "$158.80"))
     }
     
     // Methods
-    func addStock(){
-        stocks.append(WatchlistProps(stockName: "Boeing", symbol: "BA", description: "Aerospace company", price: "$141.53"))
+    func addStock() {
+        stocks.append(StockProps(stockName: "Boeing", symbol: "BA", description: "Aerospace company", price: "$141.53"))
     }
     
-    func deleteStock(index: IndexSet){ // Removes the stock from the watchlist
+    func deleteStock(index: IndexSet) { // Removes the stock from the watchlist
         stocks.remove(atOffsets: index)
     }
     
-    func createNewWatchlist(){ // Create more than one wathchlist
-        
+    func addButton() { // Opens up the searchStock
+            
     }
     
-    func deleteWatchlist(){ // Deletes a specific watchlist
-        
+    func filterWatchlist() { // Filters the watchlist by low/high price
     }
     
-    func filterWatchlist(){ // Filters the watchlist by low/high price
-        
-    }
-    
-    func moveStock(offset: IndexSet, index: Int){ // Organizes the watchlist however the users wants
+    func moveStock(offset: IndexSet, index: Int) { // Organizes the watchlist however the users wants
         stocks.move(fromOffsets: offset, toOffset: index)
-        }
+    }
 }
--- UnitedStock/WatchlistView.swift
+++ UnitedStock/WatchlistView.swift
@@ -6,20 +6,19 @@
 //
 //
 
-
 import SwiftUI
 
-struct WatchlistView: View{
+struct WatchlistView: View {
     @EnvironmentObject var stockManager: Watchlist
-    var body: some View{
-        NavigationView{
-            VStack{
-                List{
-                    ForEach(stockManager.stocks){
+    @State var searchStock = ""
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(stockManager.stocks) {
                         WatchlistStocks in
-                        HStack{
-                            // NavigationLink for more details about the stock
-                            NavigationLink(destination: Text("\(WatchlistStocks.stockName) view \n Description: \(WatchlistStocks.description)").multilineTextAlignment(.center)){
+                        HStack {
+                            NavigationLink(destination: Text("\(WatchlistStocks.stockName) view \n Description: \(WatchlistStocks.description)").multilineTextAlignment(.center)) {
                                 Text(WatchlistStocks.stockName)
                                 Spacer()
                                 Text(WatchlistStocks.price)
@@ -29,15 +28,57 @@ struct WatchlistView: View{
                     }.onDelete(perform: stockManager.deleteStock)
                         .onMove(perform: stockManager.moveStock)
                 }
-            }.navigationTitle("Watchlist")
+            }.navigationBarTitle(Text("Watchlist"))
                 .navigationBarItems(leading: EditButton())
+                .toolbar {
+                    Button {} label: {
+                        Label("Add Stock", systemImage: "plus")
+                    }
+                }
+                .searchable(text: $searchStock, placement: .navigationBarDrawer(displayMode: .always))
         }
     }
-}
 
-struct WatchlistView_Previews: PreviewProvider {
-    static var previews: some View {
-        WatchlistView()
-            .preferredColorScheme(.dark)
+    struct AddStocksView: View {
+        @SceneStorage("Stock Name") var addStockName: String = ""
+        @EnvironmentObject var stockManager: Watchlist
+        var body: some View {
+            NavigationView {
+                VStack {
+                    HStack {
+                        Text("Stock Submission")
+                            .bold()
+                            .font(.largeTitle)
+                    }
+                    .padding(.bottom, 30)
+
+                    HStack {
+                        Text("Stock Name")
+                            .bold()
+                        Spacer()
+                    }
+                    .padding(.bottom, 5)
+                    HStack {
+                        TextField("Stock Name", text: $addStockName)
+                        Spacer()
+                    }
+                    .padding(.bottom, 20)
+                    Button(action: {
+                        stockManager.stocks.append(StockProps(stockName: addStockName, symbol: "", description: "", price: ""))
+                        addStockName = ""
+                    }) {
+                        Text("Submit")
+                    }
+                    Spacer()
+                }
+                .padding()
+            }
+        }
+    }
+
+    struct WatchlistView_Previews: PreviewProvider {
+        static var previews: some View {
+            WatchlistView()
+        }
     }
 }
